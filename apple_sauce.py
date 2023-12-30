70ff31fd2de0eae85184111c7a32f6a3916db546
Ruby-L-i-n <122896514+Ruby-L-i-n@users.noreply.github.com>
2023-12-30 19:36:45 +0800

    create background

--- apple_sauce.py
+++ apple_sauce.py
@@ -12,6 +12,14 @@ pygame.display.set_caption("Applesauce Run!")
 #create a clock object 
 clock = pygame.time.Clock()
 
+#test
+sky_surface = pygame.image.load('graphics/sky.jpg')
+sky_surface = pygame.transform.scale(sky_surface, (800, 400))
+
+road_surface = pygame.image.load('graphics/road.png')
+road_surface = pygame.transform.scale(road_surface, (800, 60))
+
+
 while True: 
     #everything here
     for event in pygame.event.get():
@@ -20,7 +28,10 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-            
+
+    screen.blit(sky_surface, (0,0))
+    screen.blit(road_surface, (0,340))
+
     #update everything
     pygame.display.update()
     #set the ceiling to 60fps
