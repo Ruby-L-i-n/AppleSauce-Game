c0fd00d076ab95cce37ce7b593b2597f9a6f910d
Ruby-L-i-n <122896514+Ruby-L-i-n@users.noreply.github.com>
2023-12-30 19:08:22 +0800

    set max fps

--- apple_sauce.py
+++ apple_sauce.py
@@ -2,15 +2,26 @@ import pygame
 from sys import exit
 
 pygame.init()
+
+#create a screen
 screen = pygame.display.set_mode((800, 400))
+
+#set title
+pygame.display.set_caption("Applesauce Run!")
+
+#create a clock object 
+clock = pygame.time.Clock()
+
 while True: 
     #everything here
-
     for event in pygame.event.get():
-        #close window 
+
+        #account for closing window 
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
             
-
+    #update everything
     pygame.display.update()
+    #set the ceiling to 60fps
+    clock.tick(60)
