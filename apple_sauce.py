33a696d70d32b38b9d123210d6b2ca9f2cad0a0a
Ruby-L-i-n <122896514+Ruby-L-i-n@users.noreply.github.com>
2023-12-31 11:56:13 +0800

    text and enemy

--- apple_sauce.py
+++ apple_sauce.py
@@ -9,6 +9,9 @@ screen = pygame.display.set_mode((800, 400))
 #set title
 pygame.display.set_caption("Applesauce Run!")
 
+#font
+test_font = pygame.font.Font('fonts/SadMorningDemoRegular.ttf', 50)
+
 #create a clock object 
 clock = pygame.time.Clock()
 
@@ -18,6 +21,12 @@ sky_surface = pygame.transform.scale(sky_surface, (800, 400))
 
 road_surface = pygame.image.load('graphics/road.png')
 road_surface = pygame.transform.scale(road_surface, (800, 60))
+
+text_surface = test_font.render('Applesauce Run', False, 'White')
+
+cat_surface = pygame.image.load('graphics/Cat.png')
+cat_surface = pygame.transform.scale(cat_surface, (80, 100))
+cat_x_position = 800
 
 
 while True: 
@@ -31,6 +40,12 @@ while True:
 
     screen.blit(sky_surface, (0,0))
     screen.blit(road_surface, (0,340))
+    screen.blit(text_surface, (250,50))
+
+    cat_x_position -= 4
+    if cat_x_position < -10: cat_x_position = 800
+
+    screen.blit(cat_surface, (cat_x_position, 300))
 
     #update everything
     pygame.display.update()
