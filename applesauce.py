bfcd807f0666d4894167b235f71868a908555736
Ruby-L-i-n <122896514+Ruby-L-i-n@users.noreply.github.com>
2024-01-01 17:45:08 +0800

    update gravity and jump

--- applesauce.py
+++ applesauce.py
@@ -16,21 +16,22 @@ test_font = pygame.font.Font('fonts/SadMorningDemoRegular.ttf', 50)
 clock = pygame.time.Clock()
 
 #test
-sky_surface = pygame.image.load('graphics/sky.jpg').convert()
-sky_surface = pygame.transform.scale(sky_surface, (800, 400))
+sky_surf = pygame.image.load('graphics/sky.jpg').convert()
+sky_surf = pygame.transform.scale(sky_surf, (800, 400))
 
-road_surface = pygame.image.load('graphics/road.png').convert()
-road_surface = pygame.transform.scale(road_surface, (800, 60))
+road_surf = pygame.image.load('graphics/road.png').convert()
+road_surf = pygame.transform.scale(road_surf, (800, 60))
 
-text_surface = test_font.render('Applesauce Run', False, 'White')
+text_surf = test_font.render('Applesauce Run', False, 'White')
 
-cat_surface = pygame.image.load('graphics/Cat.png').convert_alpha()
-cat_surface = pygame.transform.scale(cat_surface, (80, 60))
-cat_rect = cat_surface.get_rect(midbottom = (800, 340))
+cat_surf = pygame.image.load('graphics/Cat.png').convert_alpha()
+cat_surf = pygame.transform.scale(cat_surf, (80, 60))
+cat_rect = cat_surf.get_rect(midbottom = (800, 340))
 
-player_surface = pygame.image.load('graphics/Jason/no_anim_0.png').convert_alpha()
-player_surface = pygame.transform.scale(player_surface, (60, 80))
-player_rect = player_surface.get_rect(midbottom = (100, 340))
+player_surf = pygame.image.load('graphics/Jason/no_anim_0.png').convert_alpha()
+player_surf = pygame.transform.scale(player_surf, (60, 80))
+player_rect = player_surf.get_rect(midbottom = (100, 340))
+player_grav = 0
 
 while True: 
     #everything here
@@ -41,18 +42,35 @@ while True:
             pygame.quit()
             exit()
 
-    screen.blit(sky_surface, (0,0))
-    screen.blit(road_surface, (0,340))
-    screen.blit(text_surface, (250,50))
+        if event.type == pygame.MOUSEBUTTONDOWN: 
+            if player_rect.bottom == 340:
+                player_grav = -15
 
-    cat_rect.left -= 4
-    if cat_rect.right < -10: cat_rect.left = 800
-    screen.blit(cat_surface, cat_rect)
-    screen.blit(player_surface, player_rect)
+        if event.type == pygame.KEYDOWN: 
+            if event.key == pygame.K_SPACE and player_rect.bottom == 340: 
+                player_grav = -15
 
+
+    #background
+    screen.blit(sky_surf, (0,0))
+    screen.blit(road_surf, (0,340))
+    screen.blit(text_surf, (250,50))
+
+    #check collision 
     if player_rect.colliderect(cat_rect) : 
         print("ahh")
         cat_rect.left = 800
+
+    #Cat
+    cat_rect.left -= 4
+    if cat_rect.right < -10: cat_rect.left = 800
+    screen.blit(cat_surf, cat_rect)
+
+    #Player
+    player_grav += 0.5
+    player_rect.y += player_grav
+    if player_rect.bottom >= 340: player_rect.bottom = 340
+    screen.blit(player_surf, player_rect)
 
     #update everything
     pygame.display.update()
